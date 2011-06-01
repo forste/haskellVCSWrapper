e9499575ebfaed6089ca7f612d504f58b4f282ac
forste <s.fortelny@gmx.at>
2011-06-01 15:39:40 +0200

    Added git bindings

---
+++ git/dist/build/autogen/Paths_git.hs
@@ -0,0 +1,29 @@
+module Paths_git (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName
+  ) where
+
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+
+version :: Version
+version = Version {versionBranch = [0,0,1], versionTags = []}
+
+bindir, libdir, datadir, libexecdir :: FilePath
+
+bindir     = "/home/n0s/.cabal/bin"
+libdir     = "/home/n0s/.cabal/lib/git-0.0.1/ghc-7.0.3"
+datadir    = "/home/n0s/.cabal/share/git-0.0.1"
+libexecdir = "/home/n0s/.cabal/libexec"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir = catch (getEnv "git_bindir") (\_ -> return bindir)
+getLibDir = catch (getEnv "git_libdir") (\_ -> return libdir)
+getDataDir = catch (getEnv "git_datadir") (\_ -> return datadir)
+getLibexecDir = catch (getEnv "git_libexecdir") (\_ -> return libexecdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
