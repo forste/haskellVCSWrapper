80d48433bdd9dcf90021263eb6da9b4611b398e0
forste <stephanfortelny@gmail.com>
2011-12-21 09:01:22 +0100

    Moved Readme and added to cabal started Mercurial

---
+++ vcswrapper/src/VCSWrapper/Mercurial.hs
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  VCSWrapper.Mercurial
+-- Copyright   :  2011 Stephan Fortelny, Harald Jagenteufel
+-- License     :  GPL Nothing
+--
+-- Maintainer  :  stephanfortelny at gmail.com, h.jagenteufel at gmail.com
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module VCSWrapper.Mercurial (
+    -- mercurial commands
+--    add
+--    ,checkout
+--    ,commit
+--    ,lock
+--    ,mergeHeadToRevision
+--    ,resolved
+--    ,simpleLog
+--    ,unlock
+--    ,update
+--    ,status
+) where
+
+
+
---
+++ vcswrapper/src/VCSWrapper/Mercurial/Process.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  VCSWrapper.Mercurial.Process
+-- Copyright   :  2011 Stephan Fortelny, Harald Jagenteufel
+-- License     :  GPL Nothing
+--
+-- Maintainer  :  stephanfortelny at gmail.com, h.jagenteufel at gmail.com
+-- Stability   :  
+-- Portability :  
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module VCSWrapper.Mercurial.Process (
+
+) where
+
+
+
