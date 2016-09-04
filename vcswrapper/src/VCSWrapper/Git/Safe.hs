1a9f7d5c014094e0e0c8f61b2984ffc68c050bdf
Jacco Krijnen <jaccokrijnen@gmail.com>
2016-09-04 12:06:27 +0200

    Add a safe git module that throws no exceptions but returns Eithers instead

--- vcswrapper/src/VCSWrapper/Git/Safe.hs
+++ vcswrapper/src/VCSWrapper/Git/Safe.hs
@@ -1,21 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 -----------------------------------------------------------------------------
 --
--- Module      :  VCSWrapper.Git
+-- Module      :  VCSWrapper.Git.Safe
 -- Copyright   :  2011 Stephan Fortelny, Harald Jagenteufel
 -- License     :  GPL
 --
--- Maintainer  :  stephanfortelny at gmail.com, h.jagenteufel at gmail.com
+-- Maintainer  :  stephanfortelny at gmail.com, h.jagenteufel at gmail.com, hamish.k.mackenzie at gmail.com
 -- Stability   :
 -- Portability :
 --
--- | Provides high-level Git functions like @commit@, @checkout@, @status@, @log@,...
+-- |
 --
--- All functions of this module run in the 'Ctx' monad, common to all VCS.
--- On unexpected behavior, these functions will throw a 'VCSException'.
 -----------------------------------------------------------------------------
 
-module VCSWrapper.Git (
+module VCSWrapper.Git.Safe (
     initDB
     , add
     , rm
@@ -52,36 +50,37 @@ import qualified Data.List
 import Data.Text (Text)
 import qualified Data.Text as T (strip, pack)
 import Data.Monoid ((<>))
+import Control.Monad (void)
 
 
 {- | Initialize a new git repository. Executes @git init@. -}
 initDB :: Bool -- ^ if 'True', this repository will be initialized as a bare repository (appends @--bare@ to the git command)
-    -> Ctx ()
+    -> Ctx (Either VCSException ())
 initDB bare = do
     let opts = if bare then ["--bare"] else []
-    gitExecWithoutResult "init-db" opts []
+    void <$> gitExec' "init-db" opts []
 
 {- | Add files to the index. Executes @git add@. -}
 add :: [ FilePath ] -- ^ 'FilePath's to add to the index.
-    -> Ctx ()
+    -> Ctx (Either VCSException ())
 add paths = do
     let opts = "--" : paths
-    gitExecWithoutResult "add" (map T.pack opts) []
+    void <$> gitExec' "add" (map T.pack opts) []
 
 {- | Remove files from the index and the working directory. Executes @git rm@. -}
 rm :: [ FilePath ] -- ^ 'FilePath's to remove.
-    -> Ctx ()
+    -> Ctx (Either VCSException ())
 rm paths = do
     let opts = "--" : map T.pack paths
-    gitExecWithoutResult "rm" opts []
+    void <$> gitExec' "rm" opts []
 
 {- | Commit the current index or the specified files to the repository. Executes @git commit@. -}
 commit :: [ FilePath ] -- ^ 'FilePath's to be commited instead of the current index. Leave empty to commit the index.
     -> Maybe (Text, Text) -- ^ (Author name, email)
     -> Text -- ^ Commit message. Don't leave this empty.
     -> [Text] -- ^ Options to be passed to the git executable.
-    -> Ctx ()
-commit rsrcs mbAuthor logmsg extraopts = do
+    -> Ctx (Either VCSException ())
+commit rsrcs mbAuthor logmsg extraopts =
         case mbAuthor of
             Just (author, author_email) ->
                 commit' (map T.pack rsrcs) logmsg extraopts ["--author", author <> " <" <> author_email <> ">"]
@@ -91,68 +90,66 @@ commit rsrcs mbAuthor logmsg extraopts = do
     commit' files logmsg extraopts authopts = do
         let msgopts = [ "-m", logmsg ]
         let opts = authopts ++ msgopts ++ extraopts ++ [ "--" ] ++ files
-        gitExecWithoutResult "commit" opts []
+        void <$> gitExec' "commit" opts []
 
 {- | Checkout the index to some commit ID. Executes @git checkout@. -}
 checkout :: Maybe Text -- ^ Commit ID
         -> Maybe Text -- ^ Branchname. If specified, @git checkout -b \<branchname\>@ will be executed.
-        -> Ctx ()
+        -> Ctx (Either VCSException ())
 checkout rev branch = do
     let bopt = maybe [] (\b -> [ "-b", b ]) branch
     let copt = maybeToList rev
-    gitExecWithoutResult "checkout" (bopt ++ copt) []
+    void <$> gitExec' "checkout" (bopt ++ copt) []
 
 {- | Return status of the repository as a list of 'Status'. Executes @git status@. -}
-status :: Ctx [Status]
+status :: Ctx (Either VCSException [Status])
 status = do
-    o <- gitExec "status" ["--porcelain"] []
-    return $ parseStatus o
+    e <- gitExec' "status" ["--porcelain"] []
+    return (fmap parseStatus e)
 
 {- | Get all commit messages. Executes @git log@. -}
 simpleLog :: Maybe Text -- ^ The branch from which to get the commit messages. (If 'Nothing', the current branch will be used).
-    -> Ctx [LogEntry]
+    -> Ctx (Either VCSException [LogEntry])
 simpleLog mbBranch = do
     -- double dash on end prevent crash if branch and filename are equal
-    o <- gitExec "log" ((branch mbBranch) ++ ["--pretty=tformat:commit:%H%n%an%n%ae%n%ai%n%s%n%b%x00", "--"]) []
-    return $ parseSimpleLog o
-    where
-    branch Nothing = []
-    branch (Just b) = [b]
-
+    e <- gitExec' "log" (maybeToList mbBranch ++ ["--pretty=tformat:commit:%H%n%an%n%ae%n%ai%n%s%n%b%x00", "--"]) []
+    return (fmap parseSimpleLog e)
 
 {- | Get all local branches. Executes @git branch@. -}
-localBranches :: Ctx (Text, [Text]) -- ^ (currently checked out branch, list of all other branches)
+localBranches :: Ctx (Either VCSException (Text, [Text])) -- ^ (currently checked out branch, list of all other branches)
 localBranches = do
-    o <- gitExec "branch" [] []
-    return $ parseBranches o
+    e <- gitExec' "branch" [] []
+    return (fmap parseBranches e)
 
 {- | Get all remotes. Executes @git remote@. -}
-remote :: Ctx [Text]
+remote :: Ctx (Either VCSException [Text])
 remote = do
-    o <- gitExec "remote" [] []
-    return $ parseRemotes o
+    e <- gitExec' "remote" [] []
+    return (fmap parseRemotes e)
 
 {- | Push changes to the remote as configured in the git configuration. Executes @git push@. -}
-push :: Ctx ()
-push = gitExecWithoutResult "push" [] []
+push :: Ctx (Either VCSException ())
+push = void <$> gitExec' "push" [] []
 
 {- | Pull changes from the remote as configured in the git configuration. If a merge conflict
     is detected, the error message is returned, otherwise 'Right ()' is returned.
     Executes @git pull@. -}
-pull :: Ctx (Either Text ())
+pull :: Ctx (Either VCSException (Maybe Text))
 pull = do
     o <- gitExec' "pull" [] []
     case o of
-        Right _                             -> return $ Right ()
-        Left exc@(VCSException _ out _ _ _) -> if (parsePullMergeConflict out) then return $ Left out
-                                            else Exc.throw exc
+        Left exc@(VCSException _ out _ _ _) | parsePullMergeConflict out ->
+            return $ Right (Just out)
+        Left e -> return (Left e)
+        Right _ -> return (Right Nothing)
+
 
 {- | Rev-parse a revision. Executes @git rev-parse@. -}
 revparse :: Text -- ^ Revision to pass to rev-parse.
-    -> Ctx (Text)
+    -> Ctx (Either VCSException Text)
 revparse commit = do
-    o <- gitExec "rev-parse" [commit] []
-    return $ T.strip o
+    o <- gitExec' "rev-parse" [commit] []
+    return $ fmap T.strip o
 
 
 
